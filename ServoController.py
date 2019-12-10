813b09a6bc0de7de8bec72243d0925820c04ece0
Sean David McAtee <sean.d.mcatee@gmail.com>
2019-12-10 12:14:42 -0800

    Initial commit

---
+++ ServoController.py
@@ -0,0 +1,9 @@
+#Uses Python3
+
+## Authors: Sean McAtee, Merai Dandouch
+## Version: 12/10/2019
+
+## About:
+#       This program is for a Raspberry Pi connected to an Adafruit Driver to control the speed of 12 servo motors.
+#       Pi: Raspberry Pi 3 Model B
+#       Driver: Adafruit CA9685 16-Channel 12-Bit PWM Driver
