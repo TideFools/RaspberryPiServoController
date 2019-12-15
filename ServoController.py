8bf906dd42d9ff7ae383ae72d49d6571ee748013
Sean David McAtee <sean.d.mcatee@gmail.com>
2019-12-15 15:41:07 -0800

    included blinkaTest

--- ServoController.py
+++ ServoController.py
@@ -1,4 +1,4 @@
-#Uses Python3
+#!/usr/bin/env python3
 
 ## Authors: Sean McAtee, Merai Dandouch
 ## Version: 12/10/2019
@@ -13,4 +13,27 @@
 #       pip install RPi.GPIO
 
 
-import RPi.GPIO as GPIO
+# Do we need to add a capacitor to our hat??
+#when setting up use 'sudo i2cdetect -y 1' to find the pin address
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+#Using BOARD mode to reference pins because it is more stable if the Pi is changed
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+#Set output channels
+chan_list = []
+
+GPIO.setup(chan_list, GPIO.OUT)
+
+#p = GPIO.PWM(channel, frequency)
+
+#dc = duty cycle between 0.0 and 100.0
+#p.start(dc)
+#p.ChangeFrequency(frequency in Hz)
+#p.ChangeDutyCycle(dc)
+#p.stop()
