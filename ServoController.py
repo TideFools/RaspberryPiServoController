5b581da48210ee9a0684d024a33aaeed32bef7c6
Sean David McAtee <sean.d.mcatee@gmail.com>
2019-12-17 17:01:08 -0800

    testing pulse lengths

--- ServoController.py
+++ ServoController.py
@@ -29,32 +29,29 @@ import Adafruit_PCA9685
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
-# Initialise the PCA9685
-#   using the default address (address=0x40)
-#   using the default bus (busnum=1)
+# Initialise the PCA9685 using the default address (address=0x40) and bus
 pwm = Adafruit_PCA9685.PCA9685()
 
 # Configure min and max servo pulse lengths
 #   pulse length out of 4096
 #   0-4 stop servos
 #   ~300 to 350 modulate speed clockwise
-#   ~354 to 400 modulate speed counterclockwise
-servo_min = 3  # Min pulse length out of 4096
-servo_max = 4  # Max pulse length out of 4096
+#   ~354 to 430 modulate speed counterclockwise
+servo_min = 300  # Min pulse length out of 4096
+servo_max = 430  # Max pulse length out of 4096
 
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-i = 500
-while i >= 400:
-    # Move servo on channel O between extremes.
+print('Moving all servos, press Ctrl-C to quit...')
+
+# Moves all servos through functional range of pulse lengths
+i = servo_min
+while i <= servo_max:
     pwm.set_all_pwm(0, i)
     print(i)
-    time.sleep(0.3)
-    #pwm.set_all_pwm(0, servo_max)
-    #time.sleep(1)
-    i = i - 1
+    time.sleep(0.01)
+    i = i + 1
 
 #stop servos with pulse 350
 pwm.set_all_pwm(0, 0)
