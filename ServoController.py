f287254512be55a8e90827d05cc7c5932c6a4784
Sean David McAtee <sean.d.mcatee@gmail.com>
2019-12-17 16:20:17 -0800

    tweaking pulse lengths

--- ServoController.py
+++ ServoController.py
@@ -35,8 +35,8 @@ import Adafruit_PCA9685
 pwm = Adafruit_PCA9685.PCA9685()
 
 # Configure min and max servo pulse lengths
-servo_min = 348  # Min pulse length out of 4096
-servo_max = 352  # Max pulse length out of 4096
+servo_min = 354  # Min pulse length out of 4096
+servo_max = 353  # Max pulse length out of 4096
 servo_stop = 350 # Holds servo in idle
 
 # Set frequency to 60hz, good for servos.
